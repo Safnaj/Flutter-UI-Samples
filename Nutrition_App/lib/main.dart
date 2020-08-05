ab1919dd59bc0bc9e87eb6e3807dbb61c300c89b
Safnaj <safnaj99@gmail.com>
2020-08-06 00:37:48 +0530

    UI Image Added

--- Nutrition_App/lib/main.dart
+++ Nutrition_App/lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(      
-     home: MyHomePage(),
-     debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -25,7 +25,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,17 +34,16 @@ class _MyHomePageState extends State<MyHomePage> {
           Padding(
             padding: EdgeInsets.only(top: 15.0, left: 10.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
-                  color: Colors.white,
-                  onPressed: () {},
-                ),
-                Container(
-                  width: 125.0,
-                  child: Row(
-                    children: <Widget>[
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                  Container(
+                    width: 125.0,
+                    child: Row(children: <Widget>[
                       IconButton(
                         icon: Icon(Icons.filter_list),
                         color: Colors.white,
@@ -56,38 +54,39 @@ class _MyHomePageState extends State<MyHomePage> {
                         color: Colors.white,
                         onPressed: () {},
                       )
-                    ]
-                  ),
-                )
-              ]
-            ),
+                    ]),
+                  )
+                ]),
           ),
-          SizedBox(height: 15.0,),
+          SizedBox(
+            height: 15.0,
+          ),
           Padding(
-            padding: EdgeInsets.only(left: 40.0),
-            child: Row(
-              children: <Widget>[
-                Text('Nutrition',
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25.0)),
-                SizedBox(width: 10.0,),
-                Text('App',
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    color: Colors.white,
-                    fontSize: 25.0))  
-              ],
-            )
-          ),
+              padding: EdgeInsets.only(left: 40.0),
+              child: Row(
+                children: <Widget>[
+                  Text('Nutrition',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0)),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('App',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontSize: 25.0))
+                ],
+              )),
           SizedBox(height: 40.0),
           Container(
             height: MediaQuery.of(context).size.height - 170.0,
             decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),            
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
             ),
             child: ListView(
               primary: false,
@@ -97,14 +96,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   padding: EdgeInsets.only(top: 40.0, bottom: 20.0),
                   child: Container(
                     height: MediaQuery.of(context).size.height - 300.0,
-                    child: ListView(
-                      children: [
-                          _buildFoodItem('assets/plate1.png', 'Salmon bowl', '\$24.00'),
-                          _buildFoodItem('assets/plate2.png', 'Spring bowl', '\$22.00'),
-                          _buildFoodItem('assets/plate6.png', 'Avocado bowl', '\$26.00'),
-                          _buildFoodItem('assets/plate5.png', 'Berry bowl', '\$24.00')
-                      ]
-                    ),
+                    child: ListView(children: [
+                      _buildFoodItem(
+                          'assets/plate1.png', 'Salmon bowl', '\$24.00'),
+                      _buildFoodItem(
+                          'assets/plate2.png', 'Spring bowl', '\$22.00'),
+                      _buildFoodItem(
+                          'assets/plate6.png', 'Avocado bowl', '\$26.00'),
+                      _buildFoodItem(
+                          'assets/plate5.png', 'Berry bowl', '\$24.00')
+                    ]),
                   ),
                 ),
                 //Checkout Row
@@ -116,10 +117,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 55.0,
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.grey,
-                          style: BorderStyle.solid,
-                          width: 1.0
-                        ),
+                            color: Colors.grey,
+                            style: BorderStyle.solid,
+                            width: 1.0),
                         borderRadius: BorderRadius.circular(10.0),
                       ),
                       child: Center(
@@ -131,11 +131,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 55.0,
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.grey,
-                          style: BorderStyle.solid,
-                          width: 1.0
-                        ),
-                        borderRadius: BorderRadius.circular(10.0), 
+                            color: Colors.grey,
+                            style: BorderStyle.solid,
+                            width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                       child: Center(
                         child: Icon(Icons.shopping_basket, color: Colors.black),
@@ -145,22 +144,18 @@ class _MyHomePageState extends State<MyHomePage> {
                       height: 55.0,
                       width: 100.0,
                       decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey,
-                          style: BorderStyle.solid,
-                          width: 1.0
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: Color(0xFF1C1428)
-                      ),
+                          border: Border.all(
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                              width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Color(0xFF1C1428)),
                       child: Center(
-                        child: Text('Checkout',
+                          child: Text('Checkout',
                               style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                color: Colors.white,
-                                fontSize: 15.0
-                              ))
-                      ),
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.white,
+                                  fontSize: 15.0))),
                     )
                   ],
                 )
@@ -178,49 +173,39 @@ class _MyHomePageState extends State<MyHomePage> {
       padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
       child: InkWell(
         onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => DetailsPage(heroTag: imgPath, foodName: foodName, foodPrice: price)
-            ));
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => DetailsPage(
+                  heroTag: imgPath, foodName: foodName, foodPrice: price)));
         },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Container(
-              child: Row(
-                children: [
-                  Hero(
+              child: Row(children: [
+                Hero(
                     tag: imgPath,
                     child: Image(
                       image: AssetImage(imgPath),
                       fit: BoxFit.cover,
                       height: 75.0,
                       width: 75.0,
-                    )
+                    )),
+                SizedBox(width: 10.0),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(
+                    foodName,
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(width: 10.0),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        foodName,
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text(
-                        price,
-                        style: TextStyle(
+                  Text(price,
+                      style: TextStyle(
                           fontFamily: 'Montserrat',
                           fontSize: 15.0,
-                          color: Colors.grey
-                        )
-                      )
-                    ]
-                  )
-                ]
-              ),
+                          color: Colors.grey))
+                ])
+              ]),
             ),
             IconButton(
               icon: Icon(Icons.add),
@@ -232,5 +217,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
 }
